6bcc8baea8d9728c55a9845d2e9fd9ae41183eca
shubhamdevpura <72294079+shubhamdevpura@users.noreply.github.com>
2020-10-18 12:10:17 +0530

    Add files via upload

---
+++ string_reverse.py
@@ -0,0 +1,8 @@
+def reverse_sentence(sentence):
+    words=sentence.split()
+    reverse_sentence=''.join(reversed(words))
+    return reverse_sentence
+
+if __name__=="__main__":
+    sentence=input("Enter sentence here:")
+    print (reverse_sentence(sentence))
